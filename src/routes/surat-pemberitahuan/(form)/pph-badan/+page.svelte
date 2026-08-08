31f12eacc875cff0ed4a3dd67c12f82e9737ad74
laynsleep <chaedar11@gmail.com>
2026-08-08 16:28:55 +0700

    pph l7

--- src/routes/surat-pemberitahuan/(form)/pph-badan/+page.svelte
+++ src/routes/surat-pemberitahuan/(form)/pph-badan/+page.svelte
@@ -16,6 +16,7 @@
 	import L4 from './components/L4/_L4.svelte';
 	import L5 from './components/L5/_L5.svelte';
 	import L6 from './components/L6/_L6.svelte';
+	import L7 from './components/L7/_L7.svelte';
 	import L10A from './components/L10-A/_L10A.svelte';
 	import L10B from './components/L10-B/_L10B.svelte';
 	import L10C from './components/L10-C/_L10C.svelte';
@@ -310,6 +311,7 @@
 				<L4 bind:currentTab/>
 				<L5 bind:currentTab/>
 				<L6 bind:currentTab/>
+				<L7 bind:currentTab/>
 				<L10A bind:currentTab/>
 				<L10B bind:currentTab/>
 				<L10C bind:currentTab/>
---
+++ src/routes/surat-pemberitahuan/(form)/pph-badan/components/L7/_L7.svelte
@@ -0,0 +1,110 @@
+<script lang="ts">
+    import Card from "$lib/components/Card.svelte";
+    import Table from "$lib/components/Table.svelte";
+    import Button from "$lib/components/Button.svelte";
+
+    interface Props {
+        currentTab: {
+            tab: string;
+            title: string;
+        }
+    }
+
+    let { currentTab = $bindable() }: Props = $props();
+
+    $effect(() => {currentTab.title = currentTab.tab === "L7" ? "PENGHITUNGAN KOMPENSASI KERUGIAN FISKAL" : currentTab.title})
+
+    const currentYear = new Date().getFullYear();
+</script>
+
+<div class="tw:mt-5 {currentTab.tab === "L7" ? "" : "tw:hidden"}">
+    <Card> 
+        {#snippet head()}
+        <span class="tw:font-bold">PENGHITUNGAN KOMPNESASI KERUGIAN FISKAL</span>
+        {/snippet}
+        {#snippet body()}
+        <Table class={"tw:w-full"}>
+            {#snippet head()}
+            <!-- svelte-ignore block_empty -->
+            {/snippet}
+            {#snippet body()}
+                <tr class="header tw:bg-[var(--color-primary)] tw:font-bold tw:text-center">
+                    <td class="tw:w-[10rem]" rowspan="3">TINDAKAN</td>
+                    <td class="tw:w-[5rem]" rowspan="3">NO.</td>
+                    <td class="tw:w-[30rem]" rowspan="2" colspan="2">LABA (RUGI) NETTO FISKAL.</td>
+                    <td class="tw:w-[75rem]" colspan="6">KOMPENSASI KERUGIAN FISKAL</td>
+                </tr>
+                <tr class="header tw:bg-[var(--color-primary)] tw:font-bold tw:text-center">
+                    <td>Y-4</td>
+                    <td>Y-3</td>
+                    <td>Y-2</td>
+                    <td>Y-1</td>
+                    <td>{currentYear}</td>
+                    <td>Y+1</td>
+                </tr>
+                <tr class="header tw:bg-[var(--color-primary)] tw:font-bold tw:text-center">
+                    <!-- LABA (RUGI) NETTO FISKAL -->
+                    <td>TAHUN/BAGIAN TAHUN PAJAK</td>
+                    <td>NILAI (Rp)</td>
+
+                    <!-- KOMPENSASI KERUGIAN FISKAL -->
+                    <td>NILAI (Rp)</td>
+                    <td>NILAI (Rp)</td>
+                    <td>NILAI (Rp)</td>
+                    <td>NILAI (Rp)</td>
+                    <td>TAHUN PAJAK INI - NILAI (Rp)</td>
+                    <td>TAHUN PAJAK BERJALAN - NILAI (Rp)</td>
+                </tr>
+                {#each {length: 10} as _, index}
+                <tr class="data tw:text-right">
+                    <td class="tw:text-center"><Button class={"tw:min-w-15!"}>Edit</Button></td>
+                    <td class="tw:text-center">{index + 1}</td>
+                    <td class="tw:text-center">{currentYear - (9 - index)}</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                {/each}
+                <tr class="footer tw:bg-[var(--color-primary)] tw:font-bold tw:text-right">
+                    <td colspan="4">JUMLAH</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+            {/snippet}
+        </Table>
+        {/snippet}
+    </Card>
+</div>
+
+<style>
+.header td, .footer td {
+    border: 1px solid white;
+}
+
+.data {
+    &:nth-child(even) {
+        background-color: #F9F6EE;
+    }
+    td {
+        padding-inline: .5rem;
+    }
+}
+
+tr {
+    border: none;
+}
+
+td {
+    padding: .5rem 1rem;
+    word-wrap: break-word;
+    font-size: .8rem;
+}
+</style>
